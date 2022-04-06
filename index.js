a185cb660bba25b49aebbb9e589c4f0964273b5a
NDICHU <ndichumuriithi@gmail.com>
2022-04-06 14:29:09 +0300

    Compleete assignment

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 
 // Write your code here
+const num1=2;
+const num2=31;
+const multiply=num1*num2;
+let random = Math.floor(Math.random()*12);
+console.log(random);
+const num3=22;
+const num4=6;
+let mod=num3%num4;
+console.log(mod);
+const max=Math.max(1,5,7,9,13,15,18,20);
